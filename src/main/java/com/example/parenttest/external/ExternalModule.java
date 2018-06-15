5b4b743f6fc6874c288c2cd39df3bf58a84b869b
Ivan Sopov <i.sopov@tinkoff.ru>
2018-06-15 11:23:05 +0300

    add prometheus, disable actuator for external context since metrics in to actuators are the same

--- src/main/java/com/example/parenttest/external/ExternalModule.java
+++ src/main/java/com/example/parenttest/external/ExternalModule.java
@@ -1,11 +1,9 @@
 package com.example.parenttest.external;
 
 import com.example.parenttest.ChildContext;
-import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @ChildContext
 public class ExternalModule {
@@ -13,24 +11,13 @@ public class ExternalModule {
 
     @Configuration
     static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        private final ManagementServerProperties managementServerProperties;
-
-        SecurityConfiguration(ManagementServerProperties managementServerProperties) {
-            this.managementServerProperties = managementServerProperties;
-        }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
                     .csrf().disable()
                     .authorizeRequests()
-                    .requestMatchers(forPort(managementServerProperties.getPort())).permitAll()
                     .anyRequest().authenticated().and().oauth2Login();
         }
-
-        private static RequestMatcher forPort(final int port) {
-            return request -> port == request.getLocalPort();
-        }
     }
-
 }
